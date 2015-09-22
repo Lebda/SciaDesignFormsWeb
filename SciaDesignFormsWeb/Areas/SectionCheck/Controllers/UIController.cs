e0c2a0ae5040e734538ee41512a2921aeaa0b492
Lebda <lebduska.jiri@seznam.cz>
2015-09-22 14:59:11 +0200

    deve

---
+++ SciaDesignFormsWeb/Areas/SectionCheck/Controllers/UIController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SciaDesignFormsWeb.Areas.SectionCheck.Controllers
+{
+    public class UIController : Controller
+    {
+        // GET: SectionCheck/UI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SciaDesignFormsWeb/Areas/SectionCheck/SectionCheckAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Areas_Help.AreaSelection;
+
+namespace SciaDesignFormsWeb.Areas.SectionCheck
+{
+    public class SectionCheckAreaRegistration : AreaRegistration 
+    {
+        public const string c_areaName = "SectionCheck";
+        public override string AreaName 
+        {
+            get { return c_areaName; }
+        }
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapHttpRoute(
+                "SectionCheck_DefaultNonRestApi",
+                AreaName + "/api/nrest/{controller}/{action}/{ClcID}/{CombiID}/{OutPutType}",
+                new { area = AreaName });
+
+            context.MapHttpRoute(
+                "SectionCheck_DefaultApi",
+                AreaName + "/api/{controller}/{id}",
+                new { area = AreaName, id = UrlParameter.Optional });
+
+            context.MapRoute(
+                "SectionCheck_default",
+                AreaName + "/{controller}/{action}/{id}",
+                new { area = AreaName, action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
