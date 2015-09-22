39e6575608d798886f87eefb8a027e092c6e74aa
Lebda <lebduska.jiri@seznam.cz>
2015-09-22 14:47:09 +0200

    deve

--- SciaDesignFormsWeb/Global.asax.cs
+++ SciaDesignFormsWeb/Global.asax.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SciaDesignFormsWeb.Infrastructure.IoC;
+using Areas_Help.AreaSelection;
 
 namespace SciaDesignFormsWeb
 {
@@ -11,11 +14,18 @@ namespace SciaDesignFormsWeb
     {
         protected void Application_Start()
         {
+            // DOne in advance
+            GlobalConfiguration.Configuration.Services.Replace(typeof(IHttpControllerSelector), new AreaHttpControllerSelector(GlobalConfiguration.Configuration));
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            NinjectResolver resolver = (NinjectResolver)GlobalConfiguration.Configuration.DependencyResolver;
+            resolver.AddBindings(Ioc_Help.Infrastructure.NinjectModule.Bindings);
+            resolver.AddBindings(SdfCalculationService.Infrastructure.NinjectModule.Bindings);
+            System.Web.Mvc.DependencyResolver.SetResolver((System.Web.Mvc.IDependencyResolver)resolver);
         }
     }
 }
---
+++ SciaDesignFormsWeb/Infrastructure/IoC/NinjectResolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Dependencies;
+using Ninject;
+
+namespace SciaDesignFormsWeb.Infrastructure.IoC
+{
+    public class NinjectResolver : System.Web.Http.Dependencies.IDependencyResolver,
+        System.Web.Mvc.IDependencyResolver
+    {
+        public NinjectResolver()
+            : this(new StandardKernel())
+        {
+        }
+
+        private readonly IKernel m_kernel;
+
+        public NinjectResolver(IKernel ninjectKernel)
+        {
+            m_kernel = ninjectKernel;
+            AddBindings(m_kernel);
+        }
+        public IDependencyScope BeginScope()
+        {
+            return this;
+        }
+        public object GetService(Type serviceType)
+        {
+            return m_kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return m_kernel.GetAll(serviceType);
+        }
+        public void Dispose()
+        {
+            // do nothing
+        }
+        public void AddBindings(params Action<IKernel>[] bindings)
+        {
+            if (bindings == null)
+            {
+                return;
+            }
+            foreach (var item in bindings)
+            {
+                item(m_kernel);
+            }
+        }
+        private void AddBindings(IKernel kernel)
+        {
+            //kernel.Bind<IRepository>().To<Repository>().InRequestScope();
+        }
+    }
+}
