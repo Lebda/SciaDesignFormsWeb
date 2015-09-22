8b321280e1cafb58c7078ff57e545df391c2885f
Lebda <lebduska.jiri@seznam.cz>
2015-09-22 16:22:32 +0200

    copy files

---
+++ SciaDesignFormsWeb/Clients/SectionCheck/documentController.js
@@ -0,0 +1,36 @@
+﻿angular.module("SectionCheck")
+    .constant("calculationDocumentUrl", '/Calculation/api/nrest/Document/')
+    .controller("documentCtrl", function ($scope, $http, calculationDocumentUrl)
+    {
+        $scope.calculationContext = new CalculationContext(2, 1, "eBrief");
+        $scope.showCanvas = false;
+
+        $scope.loadDocument = function (calcContext)
+        {
+            var url4Job = calculationDocumentUrl + 'Get/' + calcContext.GetClcID() + '/' + calcContext.GetCombiID() + '/' + calcContext.GetOutPutType();
+            $http.get(url4Job)
+                .success(function (data)
+                {
+                    var ctx = document.getElementById("canvas").getContext("2d");
+                    ctx.canvas.width = data.CanvasWidth;
+                    ctx.canvas.height = data.CanvasHeight;
+                    data.Pictures.forEach(function (item, index, array)
+                    {
+                        var imgFromWebApi = new Image();
+                        imgFromWebApi.src = "data:image/gif;base64, " + item.Base64String;
+                        ctx.drawImage(imgFromWebApi, item.Location.BoundsLocationX, item.Location.BoundsLocationY, item.Location.BoundsWidth, item.Location.BoundsHeight);
+                    });
+                    $scope.showCanvas = true;
+                })
+                .error(function ()
+                {
+                    $scope.showCanvas = false;
+                    $scope.error = "An Error has occured while loading posts!";
+                });
+        }
+
+        $scope.clearDocument = function ()
+        {
+            $scope.showCanvas = false;
+        }
+    });
